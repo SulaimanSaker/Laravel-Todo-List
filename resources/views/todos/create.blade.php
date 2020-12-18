6fac59f5a6a304aa858f78cf0250898a54005ec5
Sulaiman Saker <sulaiman.saker@hotmail.com>
2020-12-18 23:53:51 +0200

    Edit, update and delete Todos

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -6,7 +6,7 @@
 {!! Form::open(['action' => 'TodosController@store', 'method' => 'POST']) !!}
 {{ Form::bsText('text') }}
 {{ Form::bsTextArea('body') }}
-{{ Form::bsTextArea('due') }}
+{{ Form::bsText('due') }}
 {{ Form::bsSubmit('Submit', ['class' => 'btn btn-primary']) }}
 {!! Form::close() !!}
 @endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -6,6 +6,14 @@
 <h1><a href="todo/{{$todo->id}}">{{$todo->text}}</a></h1>
 <div class="label label-danger">{{$todo->due}}</div>
 <hr>
-<p>{{$todo->body}}</p>
 
+<p>{{$todo->body}}</p>
+<br><br>
+
+<a href="/todo/{{$todo->id}}/edit" class="btn btn-default">Edit</a>
+
+{!! Form::open(['action' => ['TodosController@destroy', $todo->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+{{ Form::bsSubmit('Delete', ['class' => 'btn btn-danger']) }}
+{{ Form::hidden('_method', 'DELETE') }}
+{!! Form::close() !!}
 @endsection
